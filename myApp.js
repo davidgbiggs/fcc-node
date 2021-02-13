2c1aacefaacc93f3c83e2aae8d86eb5cee51f047
David Biggs <biggs.dg@gmail.com>
2021-02-13 12:10:24 -0600

    Changed app to serve html file.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+// app.use(express.static(path.join(__dirname, "public")));
+
+// Normal usage
+app.use(express.static(__dirname + "/"));
+
 app.get("/", (req, res) => {
   path = __dirname + "/views/index.html";
   res.sendFile(path);
