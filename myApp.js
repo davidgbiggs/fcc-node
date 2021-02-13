92affdc96a8e2cbd3e183008d29e099334fb8144
David Biggs <biggs.dg@gmail.com>
2021-02-13 16:20:06 -0600

    Completing FCC challenge.

--- myApp.js
+++ myApp.js
@@ -24,4 +24,15 @@ app.get("/json", (req, res) => {
   res.json({ message: message });
 });
 
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
 module.exports = app;
