d922fc25679398719e14b1fdafdb4f4eb2e566d5
David Biggs <biggs.dg@gmail.com>
2021-02-13 15:34:18 -0600

    Completing FCC challenge.

--- myApp.js
+++ myApp.js
@@ -8,13 +8,17 @@ dotenv.config();
 // Normal usage
 app.use(express.static(__dirname + "/"));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   path = __dirname + "/views/index.html";
   res.sendFile(path);
 });
 
 app.get("/json", (req, res) => {
-  console.log(process.env.MESSAGE_STYLE);
   const message =
     process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({ message: message });
